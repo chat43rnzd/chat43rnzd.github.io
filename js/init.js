2751d29437202b1b17ee3a059d9e8e6986eab1d5
chat43rnzd <153544361+chat43rnzd@users.noreply.github.com>
2023-12-11 19:56:54 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,11 +2,11 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-  apiKey: "AIzaSyChvJlisz2W9pdz47RFegeTaB34Eujbuys",
-  authDomain: "chat153-9f6fc.firebaseapp.com",
-  projectId: "chat153-9f6fc",
-  storageBucket: "chat153-9f6fc.appspot.com",
-  messagingSenderId: "715846614215",
-  appId: "1:715846614215:web:2b243f23a2804ce53252e2",
-  measurementId: "G-LL2TLXC3L7"
+ apiKey: "AIzaSyAmNijDyZfD3jIrKhuSP72hdl6f8zQ9FMo",
+  authDomain: "caedsf.firebaseapp.com",
+  projectId: "caedsf",
+  storageBucket: "caedsf.appspot.com",
+  messagingSenderId: "503043319058",
+  appId: "1:503043319058:web:cec102d4042c677d6f3fcb",
+  measurementId: "G-9SNL31HH2G"
    });
